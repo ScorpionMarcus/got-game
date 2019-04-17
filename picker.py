251ba6d8f9923f29d83819cf44a4e6302331ae1b
Marcus LeGault <marcus.legault@scorpion.co>
2019-04-16 21:54:20 -0700

    added log files for past assignments

--- picker.py
+++ picker.py
@@ -1,10 +1,12 @@
 import random
+import datetime
 
 characters = []
 dawgs = []
 
 d = open('dawgs.txt', 'r').readlines()
 c = open('characters.txt', 'r').readlines()
+w = open('logs.txt', 'a')
 
 def removeBreak(read, arr):
     for i in read:
@@ -14,8 +16,11 @@ def picker():
     for i in dawgs:
         x = random.choice(characters)
         print(i + ': ' + x)
+        w.write(i + ': ' + x + '\n')
         characters.remove(x)
 
 removeBreak(d, dawgs)
 removeBreak(c, characters)
+w.write(str(datetime.datetime.now()) + '\n')
 picker()
+w.write('\n')
